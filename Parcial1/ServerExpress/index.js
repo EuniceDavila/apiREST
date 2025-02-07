784b675580ac32a33e024095bed8a1c743ac28f0
Eunice <davilaeunice3@gmail.com>
2025-02-07 09:12:02 -0600

    Middleware

--- Parcial1/ServerExpress/index.js
+++ Parcial1/ServerExpress/index.js
@@ -1,12 +1,39 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 const port=3000;
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname+'/public/index.html');
-    //res.send('Hola mundo');
+//Middleware de aplicación
+/*app.use('/', (req, res, next) => {
+    console.log("Petición al server");
+    next();
+}, (req, res, next) => {
+    console.log("2da función Middleware");
+    next();
+});*/
+
+//Middleware incorporado en express
+app.use(express.json());
+app.use(express.text());
+
+app.get('/alumnos', (req, res,next) => {
+    console.log(req.query);
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.patch('/', (req, res) => {
+    console.log(req.body);
+    res.send('Hola!');
+});
+
+app.post('/sistemas/:control', (req, res) => {
+    console.log(req.params);
+    res.send('Hola!');
+});
+
+app.use((req, res) => {
+    res.status(404).send("Error 404");
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`); 
+    console.log(`Server running at http://localhost:${port}`);
 });
