11a8103fea039caa9f941ddc81b1f23d8688ddac
Eunice <davilaeunice3@gmail.com>
2025-02-13 21:10:04 -0600

    ServidorExpress

--- Parcial1/ServerExpress/index.js
+++ Parcial1/ServerExpress/index.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const app = express();
-const xmlparser = require('express-xml-bodyparser');
-const port=3000;
+const express = require('express'); // Importamos Express
+const multer  = require('multer'); // Importamos Multer para manejar archivos
+const xmlparser = require('express-xml-bodyparser'); // Middleware para parsear XML
+const path = require('path'); // Módulo para trabajar con rutas
+const app = express(); // Creamos una instancia de Express
+const port = 3000; // Definimos el puerto del servidor
 
-//Middleware de aplicación
-/*app.use('/', (req, res, next) => {
-    console.log("Petición al server");
-    next();
+// Middleware de aplicación (se ejecuta en todas las peticiones)
+app.use('/', (req, res, next) => {
+    console.log("Petición al server"); // Muestra mensaje en consola
+    next(); // Llama al siguiente middleware
 }, (req, res, next) => {
-    console.log("2da función Middleware");
-    next();
-});*/
+    console.log("2da función Middleware"); // Mensaje adicional en consola
+    next(); // Continúa con la ejecución
+});
 
-//Middleware incorporado en express
-app.use(express.json());
-app.use(express.text());
-app.use(xmlparser());
+// Middleware incorporado en Express
+app.use(express.json()); // Permite recibir datos en formato JSON
+app.use(express.text()); // Permite recibir datos en formato de texto plano
+app.use(xmlparser()); // Permite recibir datos en formato XML
 
+const folder = path.join(__dirname+'/archivoRecibidos/'); 
+ const upload = multer({dest:folder});  
+app.use(upload.single('archivo'));
 
 app.get('/alumnos', (req, res,next) => {
     console.log(req.query);
@@ -34,12 +39,17 @@ app.post('/sistemas/:control', (req, res) => {
 });
 
 app.post('/prefectos', (req, res) => {
-    console.log(req.body);
-    res.send('Hola!');
+   // console.log(req.body);
+    //res.send('Hola!');
+    console.log(`Se recibio el archivo: ${req.file.originalname}`); 
+    console.log(req.body); 
+    console.log('Se recibio el formulario:'+JSON.stringify(req.body)); 
+    res.json(req.body); 
 });
 
+// Middleware para manejar rutas no encontradas (Error 404)
 app.use((req, res) => {
-    res.status(404).send("Error 404");
+    res.status(404).send("Error 404"); // Responde con un mensaje de error
 });
 
 app.listen(port, () => {
